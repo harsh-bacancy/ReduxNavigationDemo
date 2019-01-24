e8834240ec2ae3b091dc4baeb3e3925d68858701
harsh-bacancy <harsh.patel@bacancytechnology.com>
2019-01-24 15:35:46 +0530

    'Nothing Change'

--- src/screen/CounterApp.js
+++ src/screen/CounterApp.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 
-
 class CounterApp extends Component {
     constructor(props) {
         super(props);
